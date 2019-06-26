cf61ce99ebbcfa14d00ea3ffcb7e05898822bf61
Leong <980159737@qq.com>
2019-06-26 10:57:37 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/index'
-import {Layout, Button} from 'ant-design-vue'
+import {Layout, Button, Icon, Menu} from 'ant-design-vue'
 import './assets/css/main.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,6 +11,8 @@ import 'font-awesome/css/font-awesome.min.css'
 Vue.config.productionTip = false
 Vue.use(Layout)
 Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Menu)
 
 /* eslint-disable no-new */
 new Vue({
